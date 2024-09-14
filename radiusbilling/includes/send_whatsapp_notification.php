6606d7667315aa7dde9e45d11aa5a2c08a1fc125
umblox <39423646+umblox@users.noreply.github.com>
2024-09-14 13:10:35 +0700

    Update send_whatsapp_notification.php

--- radiusbilling/includes/send_whatsapp_notification.php
+++ radiusbilling/includes/send_whatsapp_notification.php
@@ -1,4 +1,14 @@
 <?php
+/*
+*******************************************************************************************************************
+* Warning!!!, Tidak untuk diperjual belikan!, Cukup pakai sendiri atau share kepada orang lain secara gratis
+*******************************************************************************************************************
+* Dibuat oleh Ikromul Umam https://t.me/arnetadotid
+*******************************************************************************************************************
+* © 2024 Arneta.ID By https://fb.me/umblox
+*******************************************************************************************************************
+*/
+
 function sendWhatsAppNotification($message, $number) {
     // Encoding URL dengan parameter message dan number
     $url = "https://wa.arneta.my.id/send-message?message=" . urlencode($message) . "&number=" . urlencode($number);
