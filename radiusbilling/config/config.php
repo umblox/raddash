aca2299dc11806b23767a233501b8832986993b1
umblox <39423646+umblox@users.noreply.github.com>
2024-09-14 09:16:04 +0700

    Update config.php
    Token

--- radiusbilling/config/config.php
+++ radiusbilling/config/config.php
@@ -1,5 +1,5 @@
 <?php
 // Global configuration
-define('BOT_TOKEN', '7530717438:AAFDpkb15BzqvjJx8-jNDT-Kg6JS0uhgbuo');
-define('ADMIN_ID', '2123457759');
+define('BOT_TOKEN', '753071xxx:tokenanda');
+define('ADMIN_ID', '2123456789xxx');
 ?>
