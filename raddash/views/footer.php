ab01c7b42e338be5707893d9c2bc7aa817f82827
umblox <39423646+umblox@users.noreply.github.com>
2024-09-17 08:04:01 +0700

    name changed

--- raddash/views/footer.php
+++ raddash/views/footer.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Billing Dashboard Pelanggan Arneta.ID</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="/radiusbilling/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/raddash/assets/css/bootstrap.min.css">
 </head>
 <body>
     <!-- Konten Halaman di sini -->
@@ -19,6 +19,6 @@
     </footer>
 
     <!-- Bootstrap JS (opsional jika diperlukan interaksi JS Bootstrap) -->
-    <script src="/radiusbilling/assets/js/bootstrap.min.js"></script>
+    <script src="/raddash/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
