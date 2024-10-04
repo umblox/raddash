fa5ac5894234f1a7a728188c45185e01ad68db10
umblox <39423646+umblox@users.noreply.github.com>
2024-10-05 01:15:43 +0700

    support trial but zero cost plan showed

--- config/prefix.php
+++ config/prefix.php
@@ -13,11 +13,12 @@ function getVoucherPrefix($planName) {
     // sesuaikan prefix voucher berdasarkan nama plan anda
     $VOUCHER_PREFIX = [
         '1Hari' => '3k',
-        '5k' => '5k',
+        'Trial' => 'trial',
         '7Hari' => '15k',
         '30Hari' => '60k'
     ];
 
+    // Untuk nama plan Trial harus sama persis dengan yang disini, dan jangan ubah prefix trial nya
     // Jika planName ditemukan, return prefix-nya; jika tidak, return default prefix
     return isset($VOUCHER_PREFIX[$planName]) ? $VOUCHER_PREFIX[$planName] : 'edit_di_config/prefix';
 }
