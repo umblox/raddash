2cc6fc0c746e994e116234ac931278f262d7217d
umblox <39423646+umblox@users.noreply.github.com>
2024-10-12 08:15:55 +0700

    Create admin_footer.php

---
+++ views/admin_footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Billing Dashboard Pelanggan Arneta.ID</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/raddash/assets/css/bootstrap.min.css">
+</head>
+<body>
+    <!-- Konten Halaman di sini -->
+
+    <footer class="bg-white text-white text-center py-3">
+        <div class="container">
+            <form action="admin.php" method="GET">
+                <button type="submit" class="btn btn-primary">Kembali ke Dashboard</button>
+            </form>
+        </div>
+    </footer>
+
+    <!-- Bootstrap JS (opsional jika diperlukan interaksi JS Bootstrap) -->
+    <script src="/raddash/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
